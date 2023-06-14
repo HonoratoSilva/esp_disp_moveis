902dd2f964e8360aee9a45149d741e9911291eeb
honoratosilva <jhsilva97@hotmail.com>
2023-06-13 21:44:00 -0300

    Atividades lógica

---
+++ lista_02/exercicio.js
@@ -0,0 +1,8 @@
+let sucessor, antecessor;
+let numero = 2;
+
+sucessor = numero + 1;
+antecessor = numero - 1;
+
+console.log(`O sucessor de ${numero} é ${sucessor} e o antecessor é ${antecessor}`);
+
